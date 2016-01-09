ff6bb3450651fb3e46df3c529befa98cfb95f1e0
Hòa Nguyễn Văn <nvh0412@gmail.com>
2016-01-09 17:56:16 +0700

    [c] fix configuration coverall

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,12 +3,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'coveralls'
+Coveralls.wear!
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
-require 'coveralls'
-Coveralls.wear!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
