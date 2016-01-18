97f5c1d7ed38e0244558e08b13ef730a1837dacd
Hòa Nguyễn Văn <nvh0412@gmail.com>
2016-01-19 02:58:33 +0700

    [f] Integrate with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   root to: 'home#index'
