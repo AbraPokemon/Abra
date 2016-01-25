50357eb45bc6e13bf6585442c2e9412084578f0c
Hòa Nguyễn Văn <nvh0412@gmail.com>
2016-01-26 00:43:05 +0700

    [f] Refactor code create and edit event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,18 +14,19 @@ class EventsController < ApplicationController
   end
 
   def create
+    debugger
     event = Event.new event_params
     event.user = current_user
-    thumbnail_file_path = "abra/event/thumbnail/#{Time.now.strftime("%Y/%m/%d/")}#{SecureRandom.hex(13)}#{File.extname(params[:event][:thumbnail].original_filename)}"
+    thumbnail_file_path = "abra/event/thumbnail/#{Time.now.strftime("%Y/%m/%d/")}#{SecureRandom.hex(13)}#{File.extname(params[:event][:thumbnail_url].original_filename)}"
     obj = S3_BUCKET.object(thumbnail_file_path)
-    obj.upload_file(params[:event][:thumbnail].tempfile, {
+    obj.upload_file(params[:event][:thumbnail_url].tempfile, {
       acl: 'public-read'
     })
 
     event.thumbnail_url = obj.public_url
 
     if event.save
-      redirect_to event
+      redirect_to event_path(event)
     else
       render 'new'  
     end
@@ -47,7 +48,10 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:name, :category_id, :location, :city, :lat, :lng, :start_at, :short_description, :number_of_participant, :required_amount, :donation_due_date, :story)
+    params.require(:event).permit(:name,
+      :category_id, :location, :city, :lat, :lng,
+      :start_at, :short_description, :number_of_participant,
+      :required_amount, :donation_due_date, :story, :thumbnail_url)
   end
 
   def get_event
