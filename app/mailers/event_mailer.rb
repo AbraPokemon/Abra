29cab058050a1d109f57e2c3087cae23c7ed50a2
Pham Quoc Duy <duypham@tinypulse.com>
2016-01-26 14:19:59 +0700

    add email for donate successful, event created, event survey

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,31 @@
+class EventMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.event_mailer.donate_successful.subject
+  #
+  def donate_successful
+    # hard-coded for first donation
+    @fd = Donation.first
+    @recommendedEvent = Event.limit 5
+    attachments.inline['charity.jpg'] = File.read(Rails.root.join('app', 'assets', 'images', 'charity.jpg'))
+
+    mail to: "to@localhost.org"
+  end
+
+  def event_created 
+    @event = Event.first
+    attachments.inline['charity.jpg'] = File.read(Rails.root.join('app', 'assets', 'images', 'charity.jpg'))
+
+    mail to: "to@localhost.org"
+  end
+
+  def event_completed_survey
+    @event = Event.first
+    attachments.inline['charity.jpg'] = File.read(Rails.root.join('app', 'assets', 'images', 'charity.jpg'))
+    attachments.inline['star.ico'] = File.read(Rails.root.join('app', 'assets', 'images', 'star.ico'))
+
+    mail to: "to@localhost.org"
+  end  
+end
